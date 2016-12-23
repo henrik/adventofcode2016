e6a8fafd18fae64050c8aa04c22cf9a4adfdcaad
Henrik Nyh <henrik@nyh.se>
2016-12-23 11:06:48 +0100

    Day 1, part 2
    Ugly-but-works solution that I might clean up later.

--- day_1/part_1_and_2.exs
+++ day_1/part_1_and_2.exs
@@ -1,13 +1,25 @@
+defmodule Location do
+  defstruct [ :x, :y ]
+
+  def all_traversed(start_location, end_location) do
+    for x <- start_location.x..end_location.x, y <- start_location.y..end_location.y do
+      %Location{x: x, y: y}
+    end -- [ end_location ]
+  end
+end
+
 defmodule State do
-  defstruct [ :facing, :x, :y ]
+  defstruct [ :facing, :location ]
 
   def update(old_state, turning, blocks) do
     direction = turn(turning, old_state.facing)
 
     %__MODULE__{
       facing: direction,
-      x: move_x(old_state.x, direction, blocks),
-      y: move_y(old_state.y, direction, blocks),
+      location: %Location{
+        x: move_x(old_state.location.x, direction, blocks),
+        y: move_y(old_state.location.y, direction, blocks),
+      },
     }
   end
 
@@ -31,15 +43,7 @@ end
 
 defmodule Day1 do
   def part1 do
-    raw_input = File.read!("input.txt")
-
-    instructions =
-      Regex.scan(~r/(L|R)(\d+)/, raw_input, capture: :all_but_first)
-      |> Enum.map(fn ([turning, blocks]) -> {turning, String.to_integer(blocks)} end)
-
-    initial_state = %State{facing: :north, x: 0, y: 0}
-
-    %State{x: final_x, y: final_y} = Enum.reduce instructions, initial_state, fn ({turning, blocks}, old_state) ->
+    %State{location: %Location{x: final_x, y: final_y}} = Enum.reduce instructions, initial_state, fn ({turning, blocks}, old_state) ->
       State.update(old_state, turning, blocks)
     end
 
@@ -48,8 +52,35 @@ defmodule Day1 do
   end
 
   def part2 do
-    answer = "???"
+    initial_visited_locations = []
+    %Location{x: final_x, y: final_y} = Enum.reduce_while instructions, {initial_state, initial_visited_locations}, fn ({turning, blocks}, {old_state, old_visited_locations}) ->
+      new_state = State.update(old_state, turning, blocks)
+
+      traversed_locations = Location.all_traversed(old_state.location, new_state.location)
+
+      previously_traversed_location = Enum.find traversed_locations, fn (loc) ->
+        Enum.member?(old_visited_locations, loc)
+      end
+
+      if previously_traversed_location do
+        {:halt, previously_traversed_location}
+      else
+        new_visited_locations = traversed_locations ++ old_visited_locations
+        {:cont, {new_state, new_visited_locations}}
+      end
+    end
+
+    answer = abs(final_x) + abs(final_y)
     IO.puts "Answer to part 2: #{answer}"
+  end
+
+  defp initial_state, do: %State{facing: :north, location: %Location{x: 0, y: 0}}
+
+  defp instructions do
+    raw_input = File.read!("input.txt")
+
+    Regex.scan(~r/(L|R)(\d+)/, raw_input, capture: :all_but_first)
+    |> Enum.map(fn ([turning, blocks]) -> {turning, String.to_integer(blocks)} end)
   end
 end
 

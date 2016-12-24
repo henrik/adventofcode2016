3c06b33c5a13aeb3f2d5754e471fa491303a86e2
Henrik Nyh <henrik@nyh.se>
2016-12-24 10:15:07 +0100

    Day 3, part 1

---
+++ day_3.exs
@@ -0,0 +1,25 @@
+defmodule Triangle do
+  def possible?([a, b, c]) when a + b > c and a + c > b and b + c > a, do: true
+  def possible?(_), do: false
+end
+
+defmodule Day3 do
+  def part1 do
+    answer =
+      input
+      |> Enum.count(&Triangle.possible?/1)
+
+    IO.puts "Number of possible triangles: #{answer}"
+  end
+
+  defp input do
+    File.read!("input/day_3.txt")
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.map(fn (line) ->
+      line |> String.split |> Enum.map(&String.to_integer/1)
+    end)
+  end
+end
+
+Day3.part1

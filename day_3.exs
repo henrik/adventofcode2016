e0ff35d6ab0abcd93696adc9e16d8e2937462840
Henrik Nyh <henrik@nyh.se>
2016-12-24 10:23:01 +0100

    Day 3, part 2

--- day_3.exs
+++ day_3.exs
@@ -12,6 +12,16 @@ defmodule Day3 do
     IO.puts "Number of possible triangles: #{answer}"
   end
 
+  def part2 do
+    answer =
+      input
+      |> Enum.chunk(3)
+      |> Enum.flat_map(fn ([[a1, b1, c1], [a2, b2, c2], [a3, b3, c3]]) -> [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]] end)
+      |> Enum.count(&Triangle.possible?/1)
+
+      IO.puts "Number of possible triangles: #{answer}"
+  end
+
   defp input do
     File.read!("input/day_3.txt")
     |> String.trim
@@ -23,3 +33,4 @@ defmodule Day3 do
 end
 
 Day3.part1
+Day3.part2

e5036902986aaa9cdc75459fb103b44b10c900f7
Henrik Nyh <henrik@nyh.se>
2016-12-24 10:35:39 +0100

    Day 3 part 2: Use List.zip

--- day_3.exs
+++ day_3.exs
@@ -1,5 +1,6 @@
 defmodule Triangle do
-  def possible?([a, b, c]) when a + b > c and a + c > b and b + c > a, do: true
+  def possible?({a, b, c}) when a + b > c and a + c > b and b + c > a, do: true
+  def possible?([a, b, c]), do: possible?({a, b, c})
   def possible?(_), do: false
 end
 
@@ -16,7 +17,7 @@ defmodule Day3 do
     answer =
       input
       |> Enum.chunk(3)
-      |> Enum.flat_map(fn ([[a1, b1, c1], [a2, b2, c2], [a3, b3, c3]]) -> [[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]] end)
+      |> Enum.flat_map(&List.zip/1)
       |> Enum.count(&Triangle.possible?/1)
 
       IO.puts "Number of possible triangles: #{answer}"
